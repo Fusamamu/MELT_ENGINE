f718ad53f78f9b1d956b7a9b89c5101ddcd61943
Sukum Duangpattra <tonypengfusama@gmail.com>
2024-05-12 14:44:46 +0700

    Git init

---
+++ cmake-build-debug/CMakeFiles/MM.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MM.dir/main.cpp.o"
+  "CMakeFiles/MM.dir/main.cpp.o.d"
+  "MM"
+  "MM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
